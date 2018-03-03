271788976372c881411fc0393c18b8fcd9fa9d51
SiaSyPeng <shiyao.peng.20@dartmouth.edu>
2018-03-03 00:22:43 -0500

    trying to fix discovery

--- js/Discovery.js
+++ js/Discovery.js
@@ -2,28 +2,37 @@ var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 var fs = require('fs');
 
 var discovery = new DiscoveryV1({
-  username: "162ce8b3-dd57-4842-8619-78aca58baa7a",
-  password: "gm7GVGfXbMA2",
-  version_date: '2017-11-07'
+  username: "162ce8b3-dd57-4842-8619-78aca58baa7a",
+  password: "gm7GVGfXbMA2",
+  version_date: '2017-11-07'
 });
 
-discovery.createEnvironment({
-  name: 'my_environment',
-  description: 'My environment',
-  size: 1
-},
-  function (err, response) {
-    if (err)
-      console.log('error:', err);
-    else
-      console.log(JSON.stringify(response, null, 2));
-});
+discovery.query({
+  environment_id: 'system', collection_id: 'news-en',
+  query: 'nested(enriched_text.entities).filter(enriched_text.entities.type::Company).filter(enriched_text.entities.sentiment.score>=0.8).term(enriched_text.entities.text)'
+}),
+  function(error, data) {
+    console.log(JSON.stringify(data, null, 2));
+  }
+);
 
-discovery.query({ environment_id: 'system', collection_id: 'news-en', "query": "\"Dartmouth \"",
-  "filter": "language:(english|en),crawl_date>2018-02-23T12:00:00-0500,crawl_date<2018-03-02T12:00:00-0500",
-  "aggregations": [
-    "term(host).term(enriched_text.sentiment.document.label)",
-    "term(enriched_text.sentiment.document.label)"
-  ] }), function(error, data) {
-  console.log(JSON.stringify(data, null, 2));
-});
+// discovery.createEnvironment({
+//   name: 'my_environment',
+//   description: 'My environment',
+//   size: 1
+// },
+//   function (err, response) {
+//     if (err)
+//       console.log('error:', err);
+//     else
+//       console.log(JSON.stringify(response, null, 2));
+// });
+// module.exports = function(req, res) {
+//
+//   discovery.query({ environment_id: 'system', collection_id: 'news-en', query:
+//       nested(enriched_text.entities).filter(enriched_text.entities.type::Company).filter(enriched_text.entities.sentiment.score>=0.8).term(enriched_text.entities.text)
+//  }), function(error, data) {
+//     console.log(JSON.stringify(data, null, 2));
+//   });
+//
+// }
